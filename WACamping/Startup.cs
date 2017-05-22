088199af8e2c49033976def7afda714d6deb0498
ferreira.damien <ferreira.damien@R20921>
2017-05-22 09:29:01 +0200

    Ajoutez des fichiers projet.

---
+++ WACamping/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WACamping.Startup))]
+namespace WACamping
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
